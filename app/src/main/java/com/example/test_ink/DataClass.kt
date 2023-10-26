0d5795cb161709dbeb0c50a72e584fb378d225d6
J0hn3ric <john.eric.arriola@gmail.com>
2023-10-26 11:55:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test_ink/DataClass.kt
@@ -0,0 +1,3 @@
+package com.example.test_ink
+
+data class DataClass(var dataText : String)
---
+++ app/src/main/java/com/example/test_ink/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.test_ink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.webkit.WebView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerViewStudi: RecyclerView
+    private lateinit var dataStudi: ArrayList<DataClass>
+    private lateinit var recyclerViewStili: RecyclerView
+    private lateinit var dataStili: ArrayList<DataClass>
+    lateinit var studiList: Array<String>
+    lateinit var stiliList: Array<String>
+    lateinit var instaView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        stiliList = resources.getStringArray(R.array.stili_nomi)
+        studiList = resources.getStringArray(R.array.studi_nomi)
+
+        recyclerViewStudi = findViewById(R.id.Studi_recyclerview)
+        recyclerViewStudi.layoutManager = LinearLayoutManager(this)
+        recyclerViewStudi.setHasFixedSize(true)
+
+        recyclerViewStili = findViewById(R.id.Stili_recyclerview)
+        recyclerViewStili.layoutManager = LinearLayoutManager(this)
+        recyclerViewStili.setHasFixedSize(true)
+
+        dataStudi = arrayListOf<DataClass>()
+        dataStili = arrayListOf<DataClass>()
+
+        getData()
+
+        instaView = findViewById(R.id.tattoo_instagram_lbl)
+        instaView.settings.javaScriptEnabled = true
+        instaView.settings.domStorageEnabled = true
+        instaView.loadUrl("https://www.instagram.com/ind.ego/")
+
+    }
+
+    private fun getData(){
+        for (i in studiList.indices){
+            val dataClassStudi = DataClass(studiList[i])
+            //val dataClassStili = DataClass(stiliList[i])
+            dataStudi.add(dataClassStudi)
+        }
+        recyclerViewStudi.adapter = AdapterClass(dataStudi)
+        for(i in stiliList.indices){
+            val dataClassStili = DataClass(stiliList[i])
+            dataStili.add(dataClassStili)
+        }
+        recyclerViewStili.adapter = AdapterClass(dataStili)
+    }
+}
